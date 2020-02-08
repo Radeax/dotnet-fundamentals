71139d266cc582270918888ef3667f0ed4472157
Radeax <joflip@gmail.com>
2020-02-08 02:27:06 -0500

    Created results action

--- DojoSurvey/Controllers/HomeController.cs
+++ DojoSurvey/Controllers/HomeController.cs
@@ -9,5 +9,14 @@ namespace DojoSurvey
     {
       return View();
     }
+    [HttpPost("result")]
+    public ViewResult Result(string name, string location, string language, string comment)
+    {
+      ViewBag.Name = name;
+      ViewBag.Location = location;
+      ViewBag.Language = language;
+      ViewBag.Comment = comment;
+      return View("Result");
+    }
   }
 }
